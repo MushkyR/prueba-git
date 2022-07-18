1f09bcfb029535bc1dd56195f28287efffb6bd10
MushkyR <moussiag@gmail.com>
2022-07-17 22:50:34 -0300

    texto en archivo F

--- F.js
+++ F.js
@@ -1,3 +1,5 @@
 let texto = "Hola, soy el archivo F"
 console.log(texto)
 
+"Buenas noches, acá ejercitando git"
+
--- a.js
+++ a.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 
 
 
 
 "archivo A probando Git"
-=======
-console.log("hola")
->>>>>>> b9b701b86daf9dab7827a1a83c0115ddb81f266d
